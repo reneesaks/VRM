ebd233ffd7a5981f3bf490701b4910469d70a720
Renee Säks <renee.saks@khk.ee>
2017-08-30 12:31:52 +0300

    Created environment for database

--- web/index.php
+++ web/index.php
@@ -1,18 +1,28 @@
  <?php
 // web/index.php
+// Using Silex framework
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-// ... definitions
+// Definitions
 $app['debug'] = true;
 
-$app->get('/hello', function () {
+/*$app->get('/hello', function () {
     return "Hello World!";
-});
+});*/
 
+// Using Twig template framework
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__.'/../views',
+]);
+
+// Using Doctrine DBAL
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+    'db.options' => [
+        'driver' => 'pdo_sqlite',
+        'path' => __DIR__.'/../database/app.db',
+    ],
 ]);
 
 $app->get('/bookings/create', function () use ($app) {
