fade71a757775fd931d0318a157a2fcbc234b348
Renee Säks <renee.saks@khk.ee>
2017-08-30 09:34:06 +0300

    Create front-controller for Silex and Bootstrap and stuff

---
+++ web/index.php
@@ -0,0 +1,15 @@
+ <?php
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+// ... definitions
+$app['debug'] = true;
+
+$app->get('/hello', function () {
+    return "Hello World!";
+});
+
+$app->run();
+?>
