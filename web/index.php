fdce4b730032726a99a99c7000f23c7c47844184
Renee Säks <renee.saks@khk.ee>
2017-08-30 13:12:00 +0300

    se composer psr-4 autoload for new namespace and create a class Application.php

--- web/index.php
+++ web/index.php
@@ -3,32 +3,9 @@
 // Using Silex framework
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
+use BookingApp\Application;
 
-// Definitions
-$app['debug'] = true;
-
-/*$app->get('/hello', function () {
-    return "Hello World!";
-});*/
-
-// Using Twig template framework
-$app->register(new Silex\Provider\TwigServiceProvider(), [
-    'twig.path' => __DIR__.'/../views',
-]);
-
-// Using Doctrine DBAL
-$app->register(new Silex\Provider\DoctrineServiceProvider(), [
-    'db.options' => [
-        'driver' => 'pdo_sqlite',
-        'path' => __DIR__.'/../database/app.db',
-    ],
-]);
-
-$app->get('/bookings/create', function () use ($app) {
-    return $app['twig']->render('base.html.twig');
-});
-
+$app = new Application();
 
 $app->run();
 ?>
